57fbac95916fc35310d52e0b4218743dfa93cc48
asaygo <andrei.saygo@gmail.com>
2018-01-28 15:58:54 -0800

    add banned api checker

---
+++ scripts/banned_api_search.py
@@ -0,0 +1,57 @@
+#! python
+###############################################
+#   .c/.cpp banned windows API checker        #
+#   Author: malwrforensics                    #
+#   Contact: malwr at malwrforensics dot com  #
+###############################################
+
+import os
+import sys
+
+banned_apis = ["strcpy", "strcpyA", "strcpyW", "wcscpy", "_tcscpy", "_mbscpy", "StrCpy", "StrCpyA", "StrCpyW", "lstrcpy", "lstrcpyA", "lstrcpyW", "_tccpy", "_mbccpy", "_ftcscpy", "strncpy", "wcsncpy", "_tcsncpy", "_mbsncpy", "_mbsnbcpy", "StrCpyN", "StrCpyNA", "StrCpyNW", "StrNCpy", "strcpynA", "StrNCpyA", "StrNCpyW", "lstrcpyn", "lstrcpynA", "lstrcpynW",
+"strcat", "strcatA", "strcatW", "wcscat", "_tcscat", "_mbscat", "StrCat", "StrCatA", "StrCatW", "lstrcat", "lstrcatA", "lstrcatW", "StrCatBuff", "StrCatBuffA", "StrCatBuffW", "StrCatChainW", "_tccat", "_mbccat", "_ftcscat", "strncat", "wcsncat", "_tcsncat", "_mbsncat", "_mbsnbcat", "StrCatN", "StrCatNA", "StrCatNW", "StrNCat", "StrNCatA", "StrNCatW", "lstrncat", "lstrcatnA", "lstrcatnW", "lstrcatn",
+"sprintfW", "sprintfA", "wsprintf", "wsprintfW", "wsprintfA", "sprintf", "swprintf", "_stprintf", "wvsprintf", "wvsprintfA", "wvsprintfW", "vsprintf", "_vstprintf", "vswprintf",
+"wvsprintf", "wvsprintfA", "wvsprintfW", "vsprintf", "_vstprintf", "vswprintf",
+"strncpy", "wcsncpy", "_tcsncpy", "_mbsncpy", "_mbsnbcpy", "StrCpyN", "StrCpyNA", "StrCpyNW", "StrNCpy", "strcpynA", "StrNCpyA", "StrNCpyW", "lstrcpyn", "lstrcpynA", "lstrcpynW", "_fstrncpy",
+"strncat", "wcsncat", "_tcsncat", "_mbsncat", "_mbsnbcat", "StrCatN", "StrCatNA", "StrCatNW", "StrNCat", "StrNCatA", "StrNCatW", "lstrncat", "lstrcatnA", "lstrcatnW", "lstrcatn", "_fstrncat",
+"gets", "_getts", "_gettws", "memcpy", "RtlCopyMemory", "CopyMemory", "wmemcpy"]
+
+def contains_banned_api(s):
+    global banned_apis
+    for api in banned_apis:
+        if api in s:
+            return 1
+    return 0
+
+def check_banned_api(fname):
+    try:
+        if len(fname) < 1:
+            return
+
+        counter_line = 0
+        with open(fname, "r") as f:
+            for line in f:
+                counter_line = counter_line + 1
+                if contains_banned_api(line) == 1:
+                    print(fname + ":(L" + str(counter_line) +"):" + line)
+    except:
+        print("check_banned_api(): Error reading from file " + str(fname))
+    return
+
+def get_files(path):
+  for root, subdirs, files in os.walk(path):
+    for file in os.listdir(root):
+        filePath = os.path.join(root, file)
+        if os.path.isdir(filePath):
+            pass
+        else:
+            if ".c" in filePath[-2:] or ".h" in filePath[-2:] or ".cpp" in filePath[-4:]:
+                print("Check: " + filePath)
+                check_banned_api(filePath)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        get_files(sys.argv[1])
+    else:
+        print("program [folder]")
